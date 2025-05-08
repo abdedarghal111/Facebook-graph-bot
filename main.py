5d1302f114d3c890600aa161d5dea452f0121508
Abderrahim Darghal Belkacemi <abdedarghal111@gmail.com>
2025-05-08 14:03:24 +0200

    feat: add and delete comments

--- main.py
+++ main.py
@@ -17,18 +17,26 @@ user = userFB.user.get_info(user_id="me")
 # obtener token de publicación
 response = requests.get(f"https://graph.facebook.com/{user.id}/accounts?access_token={os.environ['USER_ACCESS_TOKEN']}")
 
-# print(response.text)
 pageFB = FacebookApi(app_id=os.environ['APP_ID'], app_secret=os.environ['APP_SECRET_TOKEN'], access_token=response.json()['data'][0]['access_token'])
 
-# exit()
-
-print(user)
+# print(user)
 
 accounts = userFB.user.get_accounts(user_id=user.id)
 # print(accounts)
 page = accounts.data[0]
-print(page)
+# print(page)
 
 # graph.get_full_connections(page.id)
 posts = pageFB.page.get_posts(object_id=page.id)
-print(posts)
+
+for post in posts.data:
+    pageFB.comment.create(object_id=post.id, message="hola")
+
+    comments = pageFB.page.get_comments(object_id=post.id)
+    for comment in comments.data:
+        print(comment)
+        # pageFB.delete_object(object_id=comment.id)
+    # pageFB.comment.get_info(comment_id=post.id)
+# print(posts, type(posts))
+
+print("Fin programa")
